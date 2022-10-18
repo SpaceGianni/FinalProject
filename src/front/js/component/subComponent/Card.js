1e8a367a02c9de126381669a576d0238835cc082
cgabrielP <cgabriel2303@gmail.com>
2022-10-18 19:57:38 +0000

    card1

--- src/front/js/component/subComponent/Card.js
+++ src/front/js/component/subComponent/Card.js
@@ -1,16 +1,72 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Card=()=> {
   return (
     <>
-      <div className="container ">
+      <div className="container">
         <div className="row">
-          <div className="col-3 ">
+          <div className="col-sm-3 h-25">
             <div className="card ">
+              <img
+                src="https://picsum.photos/id/237/300"
+                className="card-img-top "
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">$1.000</h5>
+                <p className="card-text">nombre</p>
+                <Link to="/demo" className="btn btn-primary rounded-pill">
+                  Ver Producto
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-3 ">
+            <div className="card">
+              <img
+                src="https://picsum.photos/id/237/300"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">$1.000</h5>
+                <p className="card-text">nombre</p>
+                <Link to="/demo" className="btn btn-primary rounded-pill">
+                  Ver Producto
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-3 ">
+            <div className="card">
+              <img
+                src="https://picsum.photos/id/237/300"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">$1.000</h5>
+                <p className="card-text">nombre</p>
+                <Link to="/demo" className="btn btn-primary rounded-pill">
+                  Ver Producto
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-3 ">
+            <div className="card">
+              <img
+                src="https://picsum.photos/id/237/300"
+                className="card-img-top"
+                alt="..."
+              />
               <div className="card-body ">
-                <img src="https://picsum.photos/200/300"/>
-                <h5 className="card-title fs-4"> Price</h5>
-                <p className="card-text"> Name </p>
+                <h5 className="card-title">$1.000</h5>
+                <p className="card-text">nombre</p>
+                <Link to="/demo" className="btn btn-primary rounded-pill">
+                  Ver Producto
+                </Link>
               </div>
             </div>
           </div>
