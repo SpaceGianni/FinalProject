eb453eb92bf74f3be6b6f93a2a0ad6a5c642ea34
ZERLLEL <greatzerllel2@gmail.com>
2022-10-20 20:18:02 +0000

    new-admin

--- src/front/js/component/sideBar.js
+++ src/front/js/component/sideBar.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./../../styles/layout.scss";
 
 export const SideBar = () => {
   return (
-    <div className='col-md-2 bg-white'>sideBar</div>
-  )
-}
-
+    <>
+      {/* <div className='col-md-2 bg-white'>sideBar</div> */}
+      <div className="sideBar">
+        <ul>
+          <li>
+            xxxxxxx
+            <Link to="">Productos publicados</Link>
+          </li>
+          <li>
+            <Link to="">Pedidos</Link>
+          </li>
+          <li>
+            <Link to="">AHistorial</Link>
+          </li>
+          <li>
+            <Link to="">Administrar cuentas</Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
