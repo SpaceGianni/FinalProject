0b7db2cc9a8d262ad1057e37e44fcc3b48ab2b4a
cgabrielP <cgabriel2303@gmail.com>
2022-10-20 16:00:18 +0000

    card completada

--- src/front/js/component/subComponent/Card.js
+++ src/front/js/component/subComponent/Card.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Card = () => {
+const Card = ({image, index}) => {
   return (
     <>
 
-          <div className="col-sm-3 ">
+          <div className="col-md-4 py-3" key={index}>
             <div className="card ">
               <img
-                src="https://picsum.photos/id/237/300"
+                src={image.url}
                 className="card-img-top "
                 alt="..."
               />
               <div className="card-body">
-                <h5 className="card-title">$1.000</h5>
-                <p className="card-text">nombre</p>
-                <Link to="/demo" className="btn btn-primary rounded-pill">
+                <h5 className="card-title">{image.title}</h5>
+                <p className="card-text">{image.name}</p>
+                <Link to="/ProDetail" className="btn btn-primary rounded-pill">
                   Ver Producto
                 </Link>
               </div>
