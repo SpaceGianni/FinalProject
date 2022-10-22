088b04c4cd754fe709e9f6ace2a5d5272c752ca6
JonaGarces <jonat.garces@gmail.com>
2022-10-22 04:38:06 +0000

    loginq

---
+++ src/front/js/component/formLogin.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+export function FormLogin() {
+  const formRef = React.useRef();
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    /*
+        1. Usamos FormData para obtener la información
+        2. FormData requiere la referencia del DOM,
+           gracias al REF API podemos pasar esa referencia
+        3. Finalmente obtenemos los datos serializados
+      */
+    const formData = new FormData(formRef.current);
+    const values = Object.fromEntries(formData);
+    // Aquí puedes usar values para enviar la información
+  }
+  return (
+    <>
+      <div className="container my-5">
+        <div className="card w-75">
+          <div className="card-header ">
+            <h5 className="card-title fw-bold fs-3">Inicio Sesion</h5>
+          </div>
+          <div className="card-body"></div>
+          <form className="row mb-3" onSubmit={handleSubmit} ref={formRef}>
+            <div>
+            <label className="col-sm-2 col-form-label px-5" htmlFor="email">
+              Correo
+            </label>
+            <div class="col-sm-12 px-5">
+              <input
+                className="form-control"
+                placeholder="juanito@example.cl"
+                id="email"
+                name="email"
+                type="email"
+              />
+            </div>
+            </div>
+            <div className="pb-5">
+            <label className="col-sm-3 col-form-label px-5" htmlFor="password">
+              Contraseña
+            </label>
+            <div class="col-sm-12 px-5">
+              <input className="form-control" id="password" name="password" type="password" />
+            </div>
+            </div>
+            <div className="card-footer d-flex justify-content-center">
+              <button type="submit" className="btn btn-success  btn-sm">
+                Ingresar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
