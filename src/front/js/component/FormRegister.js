e2333ae69e3e635218297d2fbe3eaf6c906cd4d6
ZERLLEL <greatzerllel2@gmail.com>
2022-10-26 22:00:59 +0000

    Register-2

--- src/front/js/component/FormRegister.js
+++ src/front/js/component/FormRegister.js
@@ -1,30 +1,32 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export function FormRegister() {
   const { store, actions } = useContext(Context);
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [pass, setPass] = useState("");
   const [nombre, setNombre] = useState("");
   const [apellido, setApellido] = useState("");
   const [confirmPass, setConfirmPass] = useState("");
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
   const navigate = useNavigate();
-  const [flag, setFlag] = useState(true)
 
-/* const formAlert = ()=>{
-  const fNombre = ()=>{
-    if(nombre === ""){
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+    /* actions.signIn(email, password, nombre, apellido, navigate)   */
+  }; // your form submit function which will invoke after successful validation
 
-    }
-  }
-  const fApellido = ()=>{}
-  const fEmail = ()=>{}
-  const fPassword = ()=>{}
-  const fCpassword = ()=>{}
-  
-}
- */
+//    check password event 
+  const password = watch('password')
+
+  console.log(watch("password")); // you can watch individual input by pass the name of the input
 
   return (
     <>
@@ -33,102 +35,195 @@ export function FormRegister() {
           <div className="card-header ">
             <h5 className="card-title fw-bold fs-3">Registro de usuario</h5>
           </div>
-          <div className="card-body"></div>
 
-          {/* input nombre */}
-          <div className="pb-5">
-            <label className="col-form-label px-5" htmlFor="nombre">
-              Nombre
-            </label>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="py-5 px-2">
+              {/* input nombre */}
+              <div className="pb-2">
+                <label className="col-form-label px-5" htmlFor="nombre">
+                  Nombre
+                </label>
 
-            <div className="col-md-12 px-5">
-            
-                <div className={"alert alert-danger"+(flag === false ? "errorState" : "")} role="alert">
-                  alberto cualquier cosa para probar
+                <div className="col-md-12 px-5">
+                  <input
+                    onChange={(e) => {
+                      setNombre(e.target.value);
+                    }}
+                    className="form-control"
+                    id="nombre"
+                    name="nombre"
+                    type="text"
+                    {...register("firstName", {
+                      required: true,
+                      maxLength: 20,
+                      minLength: 2,
+                      pattern: /^[a-zA-Z]+$/,
+                    })}
+                  />
+                  {errors?.firstName?.type === "required" && (
+                    <div className="statusError mt-1">
+                      Debes llenar este campo
+                    </div>
+                  )}
+                  {errors?.firstName?.type === "pattern" && (
+                    <div className="statusError mt-1">
+                      Este campo solo acepta nombre sin espacio y sin numeros
+                    </div>
+                  )}
+                  {errors?.firstName?.type === "maxLength" && (
+                    <div className="statusError mt-1">
+                      Este campo no puede ser mayor a 20 caracteres
+                    </div>
+                  )}
+                  {errors?.firstName?.type === "minLength" && (
+                    <div className="statusError mt-1">
+                      Este campo no puede ser menor a 2 caracteres
+                    </div>
+                  )}
                 </div>
+              </div>
+              {/* input Apellido */}
+              <div className="pb-2">
+                <label className="col-form-label px-5" htmlFor="lastName">
+                  Apellido
+                </label>
 
-              <input
-                onChange={(e) => {
-                  setNombre(e.target.value);
-                }}
-                className="form-control"
-                id="nombre"
-                name="nombre"
-                type="text"
-              />
-            </div>
-          </div>
-          {/* input Apellido */}
-          <div className="pb-5">
-            <label className="col-form-label px-5" htmlFor="lastName">
-              Apellido
-            </label>
+                <div className="col-md-12 px-5">
+                  <input
+                    onChange={(e) => {
+                      setApellido(e.target.value);
+                    }}
+                    className="form-control"
+                    id="lastName"
+                    name="lastName"
+                    type="text"
+                    {...register("lastName", {
+                      required: true,
+                      maxLength: 20,
+                      minLength: 2,
+                      pattern: /^[A-Za-z]+$/i,
+                    })}
+                  />
+                  {errors?.lastName?.type === "required" && (
+                    <div className="statusError mt-1">
+                      Debes llenar este campo
+                    </div>
+                  )}
+                  {errors?.lastName?.type === "pattern" && (
+                    <div className="statusError mt-1">
+                      Este campo solo acepta nombre sin espacio y sin numeros
+                    </div>
+                  )}
+                  {errors?.lastName?.type === "maxLength" && (
+                    <div className="statusError mt-1">
+                      Este campo no puede ser mayor a 20 caracteres
+                    </div>
+                  )}
+                  {errors?.lastName?.type === "minLength" && (
+                    <div className="statusError mt-1">
+                      Este campo no puede ser menor a 2 caracteres
+                    </div>
+                  )}
+                </div>
+              </div>
+              {/* input correo */}
+              <div className="pb-2">
+                <label className="col-form-label px-5" htmlFor="email">
+                  Correo
+                </label>
+                <div className="col-md-12 px-5">
+                  {store.errors !== "" && (
+                    <div className="alert alert-danger" role="alert">
+                      {store.errors?.mensaje}
+                    </div>
+                  )}
+                  <input
+                    className="form-control"
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                    placeholder="juanito@example.cl"
+                    id="email"
+                    name="email"
+                    type="email"
+                    {...register("eMail", {
+                      required: true,
+                      maxLength: 60,
+                      minLength: 2,
+                      pattern: /^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/,
+                    })}
+                  />
+                  {errors?.eMail?.type === "required" && (
+                    <div className="statusError mt-1">
+                      Debes llenar este campo
+                    </div>
+                  )}
+                  {errors?.eMail?.type === "pattern" && (
+                    <div className="statusError mt-1">
+                      Este campo solo acepta una direccion de correo valida con
+                      @ incluido
+                    </div>
+                  )}
+                  {errors?.eMail?.type === "maxLength" && (
+                    <div className="statusError mt-1">
+                      Este campo no puede ser mayor a 50 caracteres
+                    </div>
+                  )}
+                </div>
+              </div>
+              {/* input contraseña */}
+              <div className="pb-2">
+                <label className="col-form-label px-5" htmlFor="password">
+                  Contraseña
+                </label>
 
-            <div className="col-md-12 px-5">
-              {/* {store.errors !== "" && (
-                <div className="alert alert-danger" role="alert">
-                  {store.errors?.mensaje}
-                </div>
-              )} */}
-              <input
-                onChange={(e) => {
-                  setApellido(e.target.value);
-                }}
-                className="form-control"
-                id="lastName"
-                name="lastName"
-                type="text"
-              />
-            </div>
-          </div>
-          {/* input correo */}
-          <div>
-            <label className="col-form-label px-5" htmlFor="email">
-              Correo
-            </label>
-            <div className="col-md-12 px-5">
-              {store.errors !== "" && (
-                <div className="alert alert-danger" role="alert">
-                  {store.errors?.mensaje}
-                </div>
-              )}
-              <input
-                className="form-control"
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                placeholder="juanito@example.cl"
-                id="email"
-                name="email"
-                type="email"
-              />
-            </div>
-          </div>
-          {/* input contraseña */}
-          <div className="pb-5">
-            <label className="col-form-label px-5" htmlFor="password">
-              Contraseña
-            </label>
+                <div className="col-md-12 px-5">
+                
+                  <input             
+                  onChange={(e) => {
+                    setPass(e.target.value);
+                  }}     
+                  placeholder="Password"
+                    className="form-control"
+                    id="password"
+                    name="password"
+                    type="text"
+                    onPaste={(e)=>{
+                      e.preventDefault()
+                      return false;
+                    }}
 
-            <div className="col-md-12 px-5">
-              {store.errors !== "" && (
-                <div className="alert alert-danger" role="alert">
-                  {store.errors?.mensaje}
+                    {...register("password", {
+                      required: true,
+                      maxLength: 16,
+                      minLength: 8,
+                      pattern: /(?=^.{8,16}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+                    })}    
+                  />
+                  {errors?.pass?.type === "required" && (
+                    <div className="statusError mt-1">
+                      Debes llenar este campo
+                    </div>
+                  )}
+                  {errors?.pass?.type === "pattern" && (
+                    <div className="statusError mt-1">
+                      Este campo debe contener letras y numeros y una mayuscula
+                    </div>
+                  )}
+                  {errors?.pass?.type === "maxLength" && (
+                    <div className="statusError mt-1">
+                      Este campo no puede ser mayor a 16 caracteres
+                    </div>
+                  )}
+                  {errors?.pass?.type === "minLength" && (
+                    <div className="statusError mt-1">
+                      Este campo no puede ser menor a 8 caracteres
+                    </div>
+                  )}
                 </div>
-              )}
-              <input
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                className="form-control"
-                id="password"
-                name="password"
-                type="password"
-              />
-            </div>
-          </div>
-          {/* input confirmar contraseña */}
-          <div className="pb-5">
+              </div>
+              {/* input confirmar contraseña */}
+              <div className="pb-2">
             <label
               className="col-form-label px-5"
               htmlFor="confirmarContraseña"
@@ -149,30 +244,43 @@ export function FormRegister() {
                 className="form-control"
                 id="confirmarContraseña"
                 name="confirmarContraseña"
-                type="password"
+                type="text"
+                {...register("confirmPassword", { required: 'confirm password is required',
+                    validate: (value) =>
+                    value === password || "The passwords do not match",
+                 })}
               />
+              {errors.confirmPassword && <span className="statusError mt-1">{errors.confirmPassword.message}</span>}
+             
             </div>
           </div>
+            </div>
 
-          <div className="card-footer d-flex justify-content-center py-3">
-            <div>
-              <button
-                type="submit"
-                onClick={() =>
-                  actions.signIn(email, password, nombre, apellido, navigate) && setFlag(false)
-                }
-                className="btn btn-success btn-md col-md-12"
-              >
-                Registrar
-              </button>
-
-              <Link to="/login">
-                <button className="btn mx-2 btn-md d-block">
-                  Volver a Login
+            <div className="card-footer d-flex justify-content-center py-3">
+              <div>
+                <button
+                  type="submit"
+                  /* onClick={() => {
+                    actions.signIn(email, password, nombre, apellido, navigate)             
+                  }} */
+                  className="btn btn-success btn-md col-12"
+                >
+                  Registrar
                 </button>
-              </Link>
+
+                <Link to="/login">
+                  <button className="btn mx-2 btn-md d-block">
+                    Volver a Login
+                  </button>
+                </Link>
+              </div>
             </div>
-          </div>
+          </form>
+          <button onClick={() => {
+                   console.log(watch('password'));         
+                  }} className="btn mx-2 btn-md d-block">
+                    xxxxx
+                  </button>
         </div>
       </div>
       {console.log(
@@ -182,7 +290,7 @@ export function FormRegister() {
         " ",
         email,
         " ",
-        password,
+        pass,
         " ",
         confirmPass
       )}
